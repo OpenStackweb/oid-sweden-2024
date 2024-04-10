46b5e9f6e15cc666b3bc198610ab4cde733820b9
Tomás Castillo <tcastilloboireau@gmail.com>
2024-04-10 19:10:32 -0300

    Adjusting styles and routes for content pages
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
     DEV_SSR: true
   },*/
   plugins: [
-    "@openeventkit/event-site"
+    "@openeventkit/event-site",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 2048,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
   ]
 };
